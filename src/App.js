84e355bd65b11c3f2bebe3eadc21b96bb8ccb330
Harsh Patel <harsh.coderc@gmail.com>
2021-05-17 17:08:06 +0530

    Added function to change user values on change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
     let currentDate = new Date();
     currentDate.setDate(currentDate.getDate() + 30);
     currentDate = currentDate.toISOString().split("T")[0];
+
+    const initialUserValues = {
+        order_no: "123abc",
+        product_name: "Watch",
+        delivery_date: currentDate,
+        delivery_time_slot: "",
+        message_card: "",
+        recipient_name: "",
+        recipient_contact_number: "",
+        recipient_address: "",
+        sender_name: "",
+        contact_number: "",
+        email_address: "testuser@yopmail.com",
+    };
+
+    const [userValues, setUserValues] = useState(initialUserValues);
+
+    const handleUserValues = (event) => {
+        const inputValue = event.target.value;
+        const inputId = event.target.id;
+        let userValuesCopy = { ...userValues };
+
+        userValuesCopy[inputId] = inputValue;
+        setUserValues(userValuesCopy);
+    };
 
     return (
         <div className="container mt-5">
@@ -19,7 +44,7 @@ const App = () => {
                         type="text"
                         className="form-control"
                         id="order_no"
-                        defaultValue="123abc"
+                        defaultValue={userValues.order_no}
                         disabled
                         readOnly
                     />
@@ -33,7 +58,7 @@ const App = () => {
                         type="text"
                         className="form-control"
                         id="product_name"
-                        defaultValue="Watch"
+                        defaultValue={userValues.product_name}
                         disabled
                         readOnly
                     />
@@ -47,17 +72,24 @@ const App = () => {
                         type="date"
                         className="form-control"
                         id="delivery_date"
-                        value={currentDate}
+                        value={userValues.delivery_date}
+                        onChange={(e) => handleUserValues(e)}
                         required
                     />
                 </div>
 
                 <div className="container mb-3">
-                    <label htmlFor="delivery_date" className="form-label">
+                    <label htmlFor="delivery_time_slot" className="form-label">
                         Delivery Time Slot
                     </label>
-                    <select class="form-select" id="delivery_date" required>
-                        <option selected disabled value="">
+                    <select
+                        className="form-select"
+                        id="delivery_time_slot"
+                        defaultValue={"DEFAULT"}
+                        onChange={(e) => handleUserValues(e)}
+                        required
+                    >
+                        <option disabled value="DEFAULT">
                             Time Slot
                         </option>
                         <option>Between 9am to 1pm</option>
@@ -77,6 +109,8 @@ const App = () => {
                         className="form-control"
                         id="message_card"
                         rows="3"
+                        value={userValues.message_card}
+                        onChange={(e) => handleUserValues(e)}
                     />
                 </div>
 
@@ -89,6 +123,8 @@ const App = () => {
                         className="form-control"
                         id="recipient_name"
                         placeholder="Enter recipient name"
+                        value={userValues.recipient_name}
+                        onChange={(e) => handleUserValues(e)}
                     />
                 </div>
 
@@ -104,6 +140,8 @@ const App = () => {
                         className="form-control"
                         id="recipient_contact_number"
                         placeholder="Enter recipient contact number"
+                        value={userValues.recipient_contact_number}
+                        onChange={(e) => handleUserValues(e)}
                         required
                     />
                 </div>
@@ -117,6 +155,8 @@ const App = () => {
                         className="form-control"
                         id="recipient_address"
                         placeholder="Enter recipient address"
+                        value={userValues.recipient_address}
+                        onChange={(e) => handleUserValues(e)}
                         required
                     />
                 </div>
@@ -130,6 +170,8 @@ const App = () => {
                         className="form-control"
                         id="sender_name"
                         placeholder="Enter sender name"
+                        value={userValues.sender_name}
+                        onChange={(e) => handleUserValues(e)}
                         required
                     />
                 </div>
@@ -143,6 +185,8 @@ const App = () => {
                         className="form-control"
                         id="contact_number"
                         placeholder="Enter contact number"
+                        value={userValues.contact_number}
+                        onChange={(e) => handleUserValues(e)}
                         required
                     />
                 </div>
@@ -155,7 +199,7 @@ const App = () => {
                         type="email"
                         className="form-control"
                         id="email_address"
-                        defaultValue="testuser@yopmail.com"
+                        defaultValue={userValues.email_address}
                         disabled
                         readOnly
                     />
